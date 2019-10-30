b483e894b250c034b4d74a515f78ed185daa46b4
anvisha16 <42858946+anvisha16@users.noreply.github.com>
2019-10-30 10:33:20 +0530

    Create SumofDigits.c

---
+++ SumofDigits.c
@@ -0,0 +1,20 @@
+# include<stdio.h> 
+  
+/* Function to get sum of digits */
+int getSum(int n) 
+{  
+   int sum = 0; 
+   while (n != 0) 
+   { 
+       sum = sum + n % 10; 
+       n = n/10; 
+   } 
+   return sum; 
+} 
+  
+int main() 
+{ 
+  int n = 687; 
+  printf(" %d ", getSum(n)); 
+  return 0; 
+} 
